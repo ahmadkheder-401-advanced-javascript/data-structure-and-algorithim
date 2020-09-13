5648bba167f4dcedb934cc94794509dee6ef005b
ahmadkheder <ahmadkhederabdulatef@gmail.com>
2020-09-14 02:19:47 +0300

    cc3-binarySearch

--- challenges/reverseArr.js
+++ challenges/reverseArr.js
@@ -11,7 +11,7 @@ function reverseArr(arr){
     let reversedArr = [];
     for(let i = 0 ; i <= arr.length-1 ; i++){
         reversedArr[i] = arr[arr.length-(i+1)]
-        // console.log(`reversedArr[${i}] = arr[${arr.length-(i+1)}]`);
+        console.log(`reversedArr[${i}] = arr[${arr.length-(i+1)}]`);
     }
     console.log('Origin Array >>>>',arr);
     console.log('Reversed Array >>>>',reversedArr);
@@ -21,16 +21,3 @@ function reverseArr(arr){
 reverseArr(orgnArr1);
 reverseArr(orgnArr2);
 reverseArr(orgnArr3);
-
-
-
-
-
-
-function reverseArr(arr){
-    let reversedArr = [];
-    for(let i = 0 ; i <= arr.length-1 ; i++){
-        reversedArr[i] = arr[arr.length-(i+1)]
-    }
-    return reversedArr;
-}
