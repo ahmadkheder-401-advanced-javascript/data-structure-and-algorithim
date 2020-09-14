0dcd45dfafb7b7523409e51b6abd70bb075f0fa5
ahmadkheder <ahmadkhederabdulatef@gmail.com>
2020-09-14 04:38:06 +0300

    ccinsertion

---
+++ challenges/ll-insertions.js
@@ -0,0 +1,24 @@
+
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+
+    let node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
