a3154a940c3822e751615bd1e8f92f2b32a7d21e
ahmadkheder <ahmadkhederabdulatef@gmail.com>
2020-10-12 03:51:59 +0300

    insertion

---
+++ react-challenges/insertionSort/insertion-sort.js
@@ -0,0 +1,18 @@
+/* eslint-disable new-cap */
+'use strict';
+ function insertionSort(arr) {
+  
+    for( let i = 1; i < arr.length;i++){
+
+        let j = i-1;
+        let temp = arr[i];
+        while(j>=0 && temp<arr[i]){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+    return arr;
+};
+
+module.exports = insertionSort;
